329b634da56488c06a671ea1bb4f6777b8faa7ef
Usman Gill <mu554024@gmail.com>
2024-01-17 12:34:55 +0500

    version0

---
+++ viteproject/src/App.jsx
@@ -0,0 +1,22 @@
+import Chai from "./chai"
+
+const anotherReactElement=(
+  <a href="https://google.com " target="blank">
+    click hare to visit to google
+  </a>
+)
+
+function App() {
+  
+
+  return (
+  // <>
+  cancelAnimationFrame
+  // <Chai/>
+  //  <h1>hello world</h1></>
+ 
+    
+  )
+}
+
+export default App
---
+++ viteproject/src/main.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+
+// const anotherReactElement=(
+//     <a href="https://google.com " target="blank">
+//       click hare to visit to google
+//     </a>
+//   )
+  const manualReact=React.createElement(
+    'a',
+    {
+        href:'https://google.com',
+        target:'blank',
+    },
+    'click hare to meet google',
+  )
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+ 
+    manualReact
+
+)
